44bf9910bd53d38e7ea78c528a33d8679e4c0e27
AnyaNek0 <16bitstreams@gmail.com>
2022-11-06 21:59:03 +0500

    build: #332dm0c: init service

---
+++ SalaryService.DataAccess/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SalaryService.DataAccess
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services)
+        {
+            services.AddSingleton<FakeDatabase>();
+
+            return services;
+        }
+    }
+}
---
+++ SalaryService.DataAccess/FakeDatabase.cs
@@ -0,0 +1,26 @@
+﻿using SalaryService.Domain;
+
+namespace SalaryService.DataAccess
+{
+    public class FakeDatabase
+    {
+        private readonly Dictionary<long, Employee> _data = new Dictionary<long, Employee>();
+
+        public void SaveAsync(Employee employee)
+        {
+            _data.Add(employee.Id, employee);
+        }
+
+        public void UpdateAsync(long employeeId, Employee employee)
+        {
+            _data[employeeId] = employee;
+        }
+
+        public Employee? GetById(long empId)
+        {
+            _data.TryGetValue(empId, out var employee);
+
+            return employee;
+        }
+    }
+}
