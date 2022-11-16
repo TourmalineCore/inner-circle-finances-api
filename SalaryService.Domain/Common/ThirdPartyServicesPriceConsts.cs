1018f46c305470289523aba59316dae81a476a4b
Anny <102854214+KAA20Work@users.noreply.github.com>
2022-11-16 12:35:04 +0300

    refactor(thirdpartyservicepriceconsts): replaced calculations with constants

--- SalaryService.Domain/Common/ThirdPartyServicesPriceConsts.cs
+++ SalaryService.Domain/Common/ThirdPartyServicesPriceConsts.cs
@@ -5,10 +5,10 @@ namespace SalaryService.Domain.Common
     {
         public const int AccountingPerYear = 7200;
 
-        public const int AccountingPerMonth = AccountingPerYear / 12;
+        public const int AccountingPerMonth = 600;
 
         public const int ParkingCostPerMonth = 1800;
 
-        public const int ParkingCostPerYear = ParkingCostPerMonth * 12;
+        public const int ParkingCostPerYear = 21600;
     }
 }
