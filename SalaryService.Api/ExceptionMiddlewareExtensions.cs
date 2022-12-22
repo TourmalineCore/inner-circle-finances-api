3db79cbac4009a5c21816c4f8dab045256353be3
AnyaNek0 <16bitstreams@gmail.com>
2022-12-22 12:36:15 +0500

    feat: #861m4cb7f: handle exceptions by ExceptionMiddlewareExtensions

---
+++ SalaryService.Api/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using SalaryService.Api.Models;
+using System.Net;
+
+namespace SalaryService.Api
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
