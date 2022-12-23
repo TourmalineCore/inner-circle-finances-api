40bb53adb1771dd399a9c63c7ee330aee45e25c2
AnyaNek0 <16bitstreams@gmail.com>
2022-12-23 12:56:03 +0500

    feat: added ceo

---
+++ SalaryService.DataAccess/Migrations/20221223075318_AddDefaultCeo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace SalaryService.DataAccess.Migrations
+{
+    public partial class AddDefaultCeo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "AccountId", "CorporateEmail", "DeletedAtUtc", "GitHub", "GitLab", "HireDate", "MiddleName", "Name", "PersonalEmail", "Phone", "Surname" },
+                values: new object[] { 50L, 1L, "ceo@tourmalinecore.com", null, "@ceo.github", "@ceo.gitlab", NodaTime.Instant.FromUnixTimeTicks(0L), "Ceo", "Ceo", "ceo@gmail.com", "88006663636", "Ceo" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 50L);
+        }
+    }
+}
