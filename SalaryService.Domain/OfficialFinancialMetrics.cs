03fc42aaa15ed6a7984c7d2e398ae6d70530e762
Vladislav Yusupov <vyusupov@tourmalinecore.com>
2023-04-17 14:24:43 +0500

    chore: #861mnqb1w: add financial metrics hirerachy

---
+++ SalaryService.Domain/OfficialFinancialMetrics.cs
@@ -0,0 +1,10 @@
+﻿namespace SalaryService.Domain
+{
+    public class OfficialFinancialMetrics : FinancialMetrics
+    {
+        public OfficialFinancialMetrics(FinancesForPayroll financesForPayroll, CoefficientOptions coefficients, WorkingPlan workingPlan) 
+            : base(financesForPayroll, coefficients, workingPlan)
+        {
+        }
+    }
+}
---
+++ SalaryService.Domain/UnofficialFinancialMetrics.cs
@@ -0,0 +1,28 @@
+﻿namespace SalaryService.Domain
+{
+    public class UnofficialFinancialMetrics : FinancialMetrics
+    {
+        public UnofficialFinancialMetrics(FinancesForPayroll financesForPayroll, CoefficientOptions coefficients, WorkingPlan workingPlan) 
+            : base(financesForPayroll, coefficients, workingPlan)
+        {
+        }
+
+        protected override decimal CalculateDistrictCoefficient() => 0;
+
+        protected override decimal CalculatePrepayment() => 0;
+
+        protected override decimal GetNdflValue() => 0;
+
+        protected override decimal GetPensionContributions() => 0;
+
+        protected override decimal GetMedicalContributions() => 0;
+
+        protected override decimal GetSocialInsuranceContributions() => 0;
+
+        protected override decimal GetInjuriesContributions() => 0;
+
+        protected override decimal CalculateGrossSalary() => 0;
+
+        protected override decimal CalculateNetSalary() => 0;
+    }
+}
