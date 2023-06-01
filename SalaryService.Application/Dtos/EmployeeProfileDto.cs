b811c5b3e9a85e1fb44dadda36e4ae6712972670
Vladislav Yusupov <vyusupov@tourmalinecore.com>
2023-06-01 08:16:32 +0500

    feat(employee-profile): #85zt6cn93: add salary info for an employee profile

--- SalaryService.Application/Dtos/EmployeeProfileDto.cs
+++ SalaryService.Application/Dtos/EmployeeProfileDto.cs
@@ -18,6 +18,14 @@ namespace SalaryService.Application.Dtos
 
         public string? GitLab { get; init; }
 
+        public decimal? FullSalary { get; init; } = null;
+
+        public decimal? DistrictCoefficient { get; init; } = null;
+
+        public decimal? IncomeTax { get; init; } = null;
+
+        public decimal? NetSalary { get; init; } = null;
+
         public EmployeeProfileDto(Employee employee)
         {
             Id = employee.Id;
@@ -27,6 +35,14 @@ namespace SalaryService.Application.Dtos
             Phone = employee.Phone;
             GitHub = employee.GitHub;
             GitLab = employee.GitLab;
+
+            if (employee.EmployeeFinancialMetrics != null)
+            {
+                FullSalary = Math.Round(employee.EmployeeFinancialMetrics.Salary, 2);
+                DistrictCoefficient = Math.Round(employee.EmployeeFinancialMetrics.DistrictCoefficient, 2);
+                IncomeTax = Math.Round(employee.EmployeeFinancialMetrics.IncomeTaxContributions, 2);
+                NetSalary = Math.Round(employee.EmployeeFinancialMetrics.NetSalary, 2);
+            }
         }
     }
 }
