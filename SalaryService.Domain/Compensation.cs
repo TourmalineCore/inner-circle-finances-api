788d9d8ace74245e2d42c2bf55960cf125ef1818
akovylyaeva <akovylyaeva@tourmalinecore.com>
2023-12-07 11:21:44 +0500

    chore: disable negative and zero amount

--- SalaryService.Domain/Compensation.cs
+++ SalaryService.Domain/Compensation.cs
@@ -29,8 +29,14 @@ public class Compensation
             throw new ArgumentException($"Compensation type [{typeId}] doesn't exists");
         }
 
+        if (amount <= 0)
+        {
+            throw new ArgumentException($"Amount can't be zero or negative");
+
+        }
+
         TypeId = typeId;
-        Comment = comment;
+        Comment = comment;     
         Amount = amount;
         IsPaid = isPaid;
         Employee = employee;
