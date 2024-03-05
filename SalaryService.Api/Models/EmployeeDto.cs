ae64dfb2ee6c5fda79a5a17df5f97d08d15e3936
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-03-05 11:00:07 +0500

    chore: change employee dto model

--- SalaryService.Api/Models/EmployeeDto.cs
+++ SalaryService.Api/Models/EmployeeDto.cs
@@ -5,14 +5,17 @@ namespace SalaryService.Api.Models;
 
 public class EmployeeDto
 {
-	public long Id { get; }
+    public long Id { get; }
 
-	public string FullName { get; }
+    public string FullName { get; }
 
-	public EmployeeDto(Employee employee)
-	{
-		Id = employee.Id;
-		FullName = employee.GetFullName();
-	}
+    public string CorporateEmail { get; }
+
+    public EmployeeDto(Employee employee)
+    {
+        Id = employee.Id;
+        FullName = employee.GetFullName();
+        CorporateEmail = employee.CorporateEmail;
+    }
 }
 
